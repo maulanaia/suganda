06222b1a85cd06527b7e25641d3bf44bdc3b102a
maulanaia <maulanaichwana@gmail.com>
2019-03-24 01:12:46 +0700

    v0.0.0324
    login.php : success with no error

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php include_once("lib/action.php");?>
+<?php include_once("lib/compInfo.php");?>
+<?php include_once("lib/pushAlert.php");?>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div id="top-alert">
+                <?php
+                    if(isset($_GET["a"])){
+                        pushAlert($_GET["a"]);
+                    }
+                ?>
+            </div>
+            <div id="login-area">
+                <?php
+                    if(isset($_POST["id"])){
+                        login($_POST["id"]);
+                    }
+                ?>
+                <form action="" method="POST">
+                    <h2><?php compTitle();?></h2>
+                    <input type="text" name="id" placeholder="ID Karyawan"/>
+                    <input type="submit" value="Masuk"/>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
